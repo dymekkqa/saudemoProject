758b78ae0c97d4b11755e80d8575b899ca88f333
dymekk <dzmitry@wolf.bet>
2023-12-16 13:29:14 +0100

    add user-agent option

--- conftest.py
+++ conftest.py
@@ -11,6 +11,9 @@ def driver(request):
             options = Options()
             options.add_argument("--headless")
             options.add_argument("--no-sandbox")
+            options.add_argument("--disable-blink-features=AutomationControlled")
+            options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+                                 "like Gecko) Chrome/120.0.0.0 Safari/537.3")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--window-size=1920,1080")
             return webdriver.Chrome(options=options)
